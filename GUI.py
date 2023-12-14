711b8d6679d49d596331ffa88355382da7a762e5
TheDrimon <135315850+TheDrimon@users.noreply.github.com>
2023-12-15 02:29:46 +0300

    Add files via upload

---
+++ GUI.py
@@ -0,0 +1,137 @@
+import pygame
+import numpy as np
+
+col = [(200,200,200), 
+        (180,180,240)]
+
+cxs = np.array([-1, 0, 1,
+				-1, 0, 1,
+				-1, 0, 1])
+cys = np.array([1, 1, 1,
+				0, 0, 0,
+				-1,-1,-1])
+
+
+class GUI_window:
+    def __init__(self, screen, Px,Py, Sx,Sy):
+        self._screen = screen
+        self.pos = (Px,Py)
+        self.size = (Sx,Sy)
+        
+    def draw(self):
+         pygame.draw.rect(self._screen,col[0],(self.pos,self.size),2)
+
+
+
+class Display(GUI_window):
+    def __init__(self, screen, Px,Py, Sx,Sy, F = np.ones((1,1,9)), Walls = np.zeros((1,1),dtype=np.bool_)):
+        super().__init__(screen, Px,Py, Sx,Sy)
+        self.F = F
+        self.Walls = Walls
+    
+    def update(self,F, cylinder):
+        self.F = F
+        self.Walls = cylinder
+    
+    def simple_draw(self):
+        self._screen.blit(pygame.transform.scale(pygame.surfarray.make_surface(self.F),self.size),self.pos)
+    
+    def get(self, x,y):
+        rho = np.sum(self.F,2)
+        ux  = np.sum(self.F*cxs,2) / rho
+        uy  = np.sum(self.F*cys,2) / rho
+        ux[self.Walls] = 0
+        uy[self.Walls] = 0
+        vorticity = (np.roll(ux, -1, axis=0) - np.roll(ux, 1, axis=0)) - (np.roll(uy, -1, axis=1) - np.roll(uy, 1, axis=1))
+
+        m = np.mean(vorticity)
+        mm = np.min(vorticity)
+    
+        K = 255*((m - vorticity)/(mm+1e-14))
+
+        W = np.zeros((len(K),len(K[0]),3))
+        W[:,:,2] = 50
+        W[:,:,0][K < 0] -= K[K < 0]
+        W[:,:,1][K > 0] += K[K > 0]
+        W[self.Walls] = (0,0,0)
+
+        return W[x,y], rho[x, y]
+        
+    def draw(self):
+        rho = np.sum(self.F,2)
+        ux  = np.sum(self.F*cxs,2) / rho
+        uy  = np.sum(self.F*cys,2) / rho
+        ux[self.Walls] = 0
+        uy[self.Walls] = 0
+        vorticity = (np.roll(ux, -1, axis=0) - np.roll(ux, 1, axis=0)) - (np.roll(uy, -1, axis=1) - np.roll(uy, 1, axis=1))
+
+        m = np.mean(vorticity)
+        mm = np.min(vorticity)
+    
+        K = 255*((m - vorticity)/(mm+1e-14))
+
+        W = np.zeros((len(K),len(K[0]),3))
+        W[:,:,2] = 50
+        W[:,:,0][K < 0] -= K[K < 0]
+        W[:,:,1][K > 0] += K[K > 0]
+        W[self.Walls] = (0,0,0)
+		
+        self._screen.blit(pygame.transform.scale(pygame.surfarray.make_surface(W),self.size),self.pos)
+        
+
+
+class Button(GUI_window):
+    def __init__(self, screen, Px,Py, Sx,Sy, text = ""):
+        super().__init__(screen, Px,Py, Sx,Sy)
+        self._text = text
+        self.pressed = 0
+        self.active = 0
+        
+    def draw(self):
+         pygame.draw.rect(self._screen,col[self.pressed],(self.pos,self.size),2+self.pressed*5)
+
+         if self._text != '':
+            M = int(min(2*self.size[0]//len(self._text),self.size[1]))# общая функция выравнивания размера 
+            T_pose = (self.pos[0]+5,self.pos[1]-3)
+
+            self._screen.blit(pygame.font.SysFont('arial', M).render(self._text, False, (250,250,250)),T_pose)
+    
+    def press(self,pressed):
+         if self.pressed and not pressed:
+             self.active = 1
+         else:
+             self.active = 0
+         self.pressed = pressed
+    
+    def reset(self):
+         self.pressed = 0
+         
+
+
+class Switch(GUI_window):
+    def __init__(self, screen, Px,Py, Sx,Sy, text = [""]):
+        super().__init__(screen, Px,Py, Sx,Sy)
+        self.pressed = 0
+        self.state = 0
+        self.active = 0
+        self._text = text
+        
+    def draw(self):
+         pygame.draw.rect(self._screen,col[self.pressed],(self.pos,self.size),2)
+
+         if self._text[self.state] != '':
+            M = int(1.9*self.size[0]//(1.4+len(self._text[self.state])**0.7))# общая функция выравнивания размера текста
+            T_pose = (self.pos[0]+5,self.pos[1]-5)
+            self._screen.blit(pygame.font.SysFont('arial', M).render(self._text[self.state], False, (250,250,250)),T_pose)
+
+    def reset(self):
+         self.pressed = 0
+    
+    def press(self,pressed):
+         if self.pressed and not pressed:
+             self.state = (self.state + 1) % len(self._text)
+             self.active = 1
+         else:
+             self.active = 0
+        
+         self.pressed = pressed
--- liquid_2d.py
+++ liquid_2d.py
@@ -1,54 +1,195 @@
 import pygame as pg
 import numpy as np
+import GUI
+
+def Field_init():
+    global NL, idxs, cxs, cys, weights
+    global F
+    NL = 9
+    idxs = np.arange(NL)
+    cxs = np.array([-1, 0, 1,
+				    -1, 0, 1,
+				    -1, 0, 1])
+    cys = np.array([1, 1, 1,
+				    0, 0, 0,
+				    -1,-1,-1])
+    weights = np.array([1/36, 1/9, 1/36,
+					    1/9,  4/9, 1/9,
+					    1/36, 1/9, 1/36])
+    
+    #####v = np.float64(np.zeros((N*He//Wi,N,2)))
+    Nx, Ny = N*He//Wi, N
+
+    # Начальные условия
+    global cylinder, rho0, tau
+	            
+    rho0 = 10 # average density
+    tau = 1 # collision timescale
+	
+    F = np.ones((Ny,Nx,NL)) * rho0 / NL
+	
+    np.random.seed(42)
+	
+    F += 0.01*np.random.randn(Ny,Nx,NL)
+	
+    X, Y = np.meshgrid(range(Nx), range(Ny))
+	
+    F[:,:,1] += 2*(1+0.2*np.cos(2*np.pi*X/Nx*4))
+	
+    rho = np.sum(F,2)
+    for i in idxs:
+        F[:,:,i] *= rho0 / rho
+
+
+    cylinder = (X - Nx/2)**2 + (Y - Ny/4)**2 < (Nx/4)**2 # Cylinder boundary
+	
+    global display
+    display.update(F, cylinder)
 
 
 def init():
-    global v, m, N, screen, Wi, He
+	# GUI init <--
+    global N, screen, Wi, He, b
+    global mode, play
+    global Buttons, display
     
-    Wi, He = 1400,1000
-    N = 300
+    play = 0
+    mode = "draw"  # draw select del 
+    Wi, He = 1080,360
+    N = 324
+    
+    
     
     pg.init()
-    screen = pg.display.set_mode((Wi,He)) 
+    b = 30
+    screen = pg.display.set_mode((Wi,He + b)) 
     
-    v = np.float64(np.zeros((N*He//Wi,N,2)))
-    #m = np.float64(np.ones((N*He//Wi,N))) вязкость
-    		
-	#Xo,Yo = 100,100
-	#v[Xo-5:Xo+5,Yo] = (1600,0)	
+    Buttons = [GUI.Button(screen, 0,He, b*2,b, "reset"),
+			   GUI.Switch(screen, b*2.1*2,He, b*2,b, ["draw","select","del"]),
+			   GUI.Switch(screen, b*2.1,He, b*2,b, ["play","pause"])]
+    display = GUI.Display(screen, 0,0, Wi,He)
+	
+    # MODEL init <--
+    Field_init()
+	
+    
 
 def step_calc():
-    global v
-    v[0,:] = (1,0)# пост усл.
-    
-    nV = v * 4/9
-    
-    nV[:-1] += v[1:] / 9
-    nV[1:] += v[:-1] / 9
-    nV[:,:-1] += v[:,1:] / 9
-    nV[:,1:] += v[:,:-1] / 9
-    
-    nV[:-1,:-1] += v[1:,1:] / 36
-    nV[:-1,1:] += v[1:,:-1] / 36
-    nV[1:,:-1] += v[-1:,1:] / 36
-    nV[1:,1:] += v[:-1,:-1] / 36
-    #FIXME должно считать по Навье-Стоксу
-	
-    v = nV
+		global cylinder, F
+		global display
+		#print(it)
 
+		# Drift
+		for i, cx, cy in zip(idxs, cxs, cys):
+			F[:,:,i] = np.roll(F[:,:,i], cx, axis=1)
+			F[:,:,i] = np.roll(F[:,:,i], cy, axis=0)
+
+
+		# Set reflective boundaries
+		bndryF = F[cylinder,:]
+		bndryF = bndryF[:,[-1,-2,-3,-4,-5,-6,-7,-8,0]]
+
+
+		# Calculate fluid variables
+		rho = np.sum(F,2)
+		ux  = np.sum(F*cxs,2) / rho
+		uy  = np.sum(F*cys,2) / rho
+
+
+		# Apply Collision
+		Feq = np.zeros(F.shape)
+		for i, cx, cy, w in zip(idxs, cxs, cys, weights):
+			ss = (cx*ux+cy*uy)
+			Feq[:,:,i] = rho * w * ( 1 + 3*ss + 9*ss**2/2 - 3*(ux*ux+uy*uy)/2 )
+
+		F += -(1.0/tau) * (F - Feq)
+
+		# Apply boundary
+		F[cylinder,:] = bndryF
+
+
+		display.update(F, cylinder)
+			
+	
+def interract(press, pos):
+	global mode, Buttons, play
+	
+	x,y = pos[:2]
+	if 0 < x < Wi:
+		if 0 < y < He:
+			# работа с полем
+			print(pos)
+			if type == "draw":
+				pass
+			if type == "select":
+				pass
+			elif type == "del":
+				pass
+        
+		elif He < y < He + b:
+			for Bt in Buttons: # переключение кнопок
+				if Bt.pos[0] < x < Bt.pos[0] + Bt.size[0]:
+					Bt.press(press)
+		
+		if press == 0:
+			for Bt in Buttons: # сброс кнопок
+				Bt.reset()
+		
+	# реакции кнопок
+	if Buttons[0].active:
+		Field_init()
+		Buttons[0].active = 0
+	if Buttons[2].active:
+		if Buttons[2].state == 0:
+			play = 0
+		elif Buttons[2].state == 1:
+			play = 1
+		Buttons[2].active = 0
+		
+			
+	
+	
+	
+
+    
 
 def draw():
 	screen.fill((0,0,0))
-	screen.blit(pg.transform.scale(pg.surfarray.make_surface(1+v.sum(axis = 2)),(Wi,He)), (20,100))# отрисовка по модулю скорости и позже вязкости
+
+	display.draw()# отрисовка по скорости кручения
+	
+	for i in Buttons:# интерфейс
+		i.draw()
+		
 	pg.display.update()
 
 
 def main():
 	init()
 	
-	while True:
-	    step_calc()
-	    draw()
+	clock = pg.time.Clock()
+	RUN = True
+	
+	while RUN:
+		if play:
+			step_calc()
+		
+		draw()
+
+		clock.tick(60)
+            
+		for event in pg.event.get():
+			if event.type == pg.QUIT:
+				RUN = False
+				
+			elif event.type == pg.MOUSEBUTTONDOWN:
+				interract(1, event.pos)
+				
+			elif event.type == pg.MOUSEBUTTONUP:
+				interract(0, event.pos)
+			
+			elif event.type == pg.MOUSEMOTION:
+				pass
 
 
 if __name__ == "__main__":
